e40f9e6cf2c285fabdb370d3be3b08e746f674cd
Dru Sellers <dru@drusellers.com>
2016-09-12 07:22:47 -0500

    adding stylecop

---
+++ dotnetlint/GlobalSuppressions.cs
@@ -0,0 +1,9 @@
+﻿
+// This file is used by Code Analysis to maintain SuppressMessage 
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given 
+// a specific target and scoped to a namespace, type, member, etc.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.ReadabilityRules", "SA1101:Prefix local calls with this", Justification = "Because that's how I roll.", Scope = "member", Target = "~M:dotnetlint.Modes.LocalFileSystem.Sources.SolutionSource.#ctor(System.String)")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.NamingRules", "SA1309:Field names must not begin with underscore", Justification = "Because that's how I roll", Scope = "member", Target = "~F:dotnetlint.Modes.LocalFileSystem.Sources.SolutionSource._input")]
+
--- dotnetlint/Modes.LocalFileSystem.Sources/SolutionSource.cs
+++ dotnetlint/Modes.LocalFileSystem.Sources/SolutionSource.cs
@@ -1,14 +1,13 @@
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using dotnetlint.Sources;
-using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CSharp;
-using Microsoft.CodeAnalysis.MSBuild;
-
 namespace dotnetlint.Modes.LocalFileSystem.Sources
 {
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using dotnetlint.Sources;
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.MSBuild;
+
     public class SolutionSource : Source
     {
         readonly string _input;
@@ -23,19 +22,19 @@ namespace dotnetlint.Modes.LocalFileSystem.Sources
             var slnPath = Path.GetFullPath(_input);
             var workspace = MSBuildWorkspace.Create();
             var solution = await workspace.OpenSolutionAsync(slnPath);
-            
+
             var results = new List<TextAndPath>();
             foreach (var project in solution.Projects)
             {
                 var compilationData = await project.GetCompilationAsync();
-                var diagnosticData = compilationData.GetDiagnostics().Where(i=>i.Severity != DiagnosticSeverity.Hidden);
-
+                var diagnosticData = compilationData.GetDiagnostics()
+                                                    .Where(i => i.Severity != DiagnosticSeverity.Hidden);
 
                 foreach (var document in project.Documents)
                 {
                     if (document.SupportsSyntaxTree)
                     {
-                        results.Add(new TextAndPath(document.GetTextAsync().Result,
+                        results.Add(new TextAndPath(await document.GetTextAsync(),
                             document.FilePath,
                             null));
                     }
