34171a56226fccaddf501ef4d9685c78e8044c5f
Dru Sellers <dru@drusellers.com>
2017-03-03 16:36:18 -0600

    stuffs

--- dotnetlint/GlobalSuppressions.cs
+++ dotnetlint/GlobalSuppressions.cs
@@ -1,9 +1,10 @@
-﻿
-// This file is used by Code Analysis to maintain SuppressMessage 
+﻿// This file is used by Code Analysis to maintain SuppressMessage
 // attributes that are applied to this project.
-// Project-level suppressions either have no target or are given 
+// Project-level suppressions either have no target or are given
 // a specific target and scoped to a namespace, type, member, etc.
 
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.ReadabilityRules", "SA1101:Prefix local calls with this", Justification = "Because that's how I roll.", Scope = "member", Target = "~M:dotnetlint.Modes.LocalFileSystem.Sources.SolutionSource.#ctor(System.String)")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.NamingRules", "SA1309:Field names must not begin with underscore", Justification = "Because that's how I roll", Scope = "member", Target = "~F:dotnetlint.Modes.LocalFileSystem.Sources.SolutionSource._input")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1400:Access modifier must be declared", Justification = "Because that's how I roll", Scope = "member", Target = "~F:dotnetlint.Modes.LocalFileSystem.Sources.SolutionSource._input")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.NamingRules", "SA1300:Element must begin with upper-case letter", Justification = "Nope", Scope = "namespace", Target = "~N:dotnetlint.Modes.LocalFileSystem.Sources")]
 
--- dotnetlint/Modes.LocalFileSystem.Sources/SolutionSource.cs
+++ dotnetlint/Modes.LocalFileSystem.Sources/SolutionSource.cs
@@ -1,3 +1,15 @@
+// Copyright 2016 Dru Sellers, Keith Dahlby, et. al.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+// this file except in compliance with the License. You may obtain a copy of the
+// License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0 
+//
+// Unless required by applicable law or agreed to in writing, software distributed
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+// specific language governing permissions and limitations under the License.
 namespace dotnetlint.Modes.LocalFileSystem.Sources
 {
     using System.Collections.Generic;
@@ -6,6 +18,7 @@ namespace dotnetlint.Modes.LocalFileSystem.Sources
     using System.Threading.Tasks;
     using dotnetlint.Sources;
     using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.CSharp;
     using Microsoft.CodeAnalysis.MSBuild;
 
     public class SolutionSource : Source
@@ -26,9 +39,13 @@ namespace dotnetlint.Modes.LocalFileSystem.Sources
             var results = new List<TextAndPath>();
             foreach (var project in solution.Projects)
             {
+                var c = (CSharpCompilationOptions) project.CompilationOptions;
+                var nc = c.WithGeneralDiagnosticOption(ReportDiagnostic.Warn);
+
                 var compilationData = await project.GetCompilationAsync();
                 var diagnosticData = compilationData.GetDiagnostics()
-                                                    .Where(i => i.Severity != DiagnosticSeverity.Hidden);
+                                                    //.Where(i => i.Severity != DiagnosticSeverity.Hidden)
+                                                    ;
 
                 foreach (var document in project.Documents)
                 {
